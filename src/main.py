69434de1b7fd04be919244ae9db4f849f63f36b1
Grace Zhang <gracewzhang01@gmail.com>
2023-12-21 15:26:05 -0800

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,31 @@
+import typer
+from typing_extensions import Annotated
+
+from quartz import Quartz
+
+app = typer.Typer()
+
+@app.command()
+def playlist(
+    url: str,
+    out_dir: Annotated[
+        str, typer.Option(help='Directory to download files to')
+    ] = '/quartz',
+):
+    quartz = Quartz(out_dir)
+    quartz.process_playlist(url)
+
+
+@app.command()
+def song(
+    url: str,
+    out_dir: Annotated[
+        str, typer.Option(help='Directory to download file to')
+    ] = '/quartz',
+):
+    quartz = Quartz(out_dir)
+    quartz.process_song(url)
+
+
+if __name__ == '__main__':
+    app()
---
+++ src/quartz.py
@@ -0,0 +1,9 @@
+class Quartz:
+    def __init__(self, out_dir: str) -> None:
+        self.out_dir = out_dir
+
+    def process_playlist(url: str, out_dir: str) -> None:
+        pass
+
+    def process_song(url: str, out_dir: str) -> None:
+        pass
